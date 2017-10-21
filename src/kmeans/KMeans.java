551eaaaec9617dd81c624512f680fad42173250e
liuxl <luxurymask@163.com>
2017-10-21 16:08:18 +0800

    .

--- src/kmeans/KMeans.java
+++ src/kmeans/KMeans.java
@@ -7,34 +7,45 @@ import java.util.List;
 
 import org.ansj.domain.Result;
 import org.ansj.library.DicLibrary;
-import org.ansj.recognition.arrimpl.UserDefineRecognition;
-import org.ansj.splitWord.analysis.DicAnalysis;
-import org.nlpcn.commons.lang.tire.domain.Value;
-import org.nlpcn.commons.lang.tire.library.Library;;
+import org.ansj.splitWord.analysis.DicAnalysis;;
 
 public class KMeans {
-	public static List<List<String>> cluster(String queryFilePath){
+	public static List<List<String>> cluster(String queryFilePath) {
 		File queryFile = new File(queryFilePath);
 		BufferedReader reader;
 		String query;
-		try{
+		try {
 			reader = new BufferedReader(new FileReader(queryFile));
-			while((query = reader.readLine()) != null){
+			while ((query = reader.readLine()) != null) {
 				DicLibrary.insert(DicLibrary.DEFAULT, "消岩汤剂");
 				Result result = DicAnalysis.parse(query);
 				System.out.println(result);
 				String[] subTexts = result.toStringWithOutNature().split(",");
-				
+
 			}
-		}catch(Exception e){
+		} catch (Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
-	
-	public static void main(String[] args){
-		String queryFilePath = "/Users/liuxl/Desktop/recommendation/phase1/input/task1.input";
-		cluster(queryFilePath);
+
+	/**
+	 * args[0]:
+	 * 查询文件文件夹路径(Mac："/Users/liuxl/Desktop/recommendation/phase1/input/task1.input"
+	 * Windows："")
+	 */
+	public static void main(String[] args) {
+
+		String queryFileFolderPath = args[0];
+		File folder = new File(queryFileFolderPath);
+		File[] fileList = folder.listFiles();
+		for (File queryFile : fileList) {
+			String queryFilePath = queryFile.getAbsolutePath();
+			if (queryFilePath.endsWith(".input")) {
+				cluster(queryFilePath);
+			}
+		}
+
 	}
 }
